61bdf511fb1526c98b17e0d9456d2db684af16dc
Mohmed <mohmedmahmoudfouad@gmail.com>
2022-06-29 23:38:18 +0200

    Protofilo-by-js

--- main.js
+++ main.js
@@ -4,97 +4,11 @@ let idcontact = document.getElementById('idcontact');
 let ideduaction = document.getElementById('ideduaction');
 let idPricing = document.getElementById('idPricing');
 let leavetotop = document.querySelector('.leavetotop');
-let offline = document.querySelector('.offline');
 let ulitems = document.querySelector('.ulitems');
 let Mohmed = document.querySelector('.Mohmed');
-let black = document.querySelector('.black');
-let dark = document.getElementById('dark');
-let fristbtn = document.querySelector('.fristbtn');
-let oneinput = document.querySelector('.oneinput');
-let twoinput = document.querySelector('.twoinput');
-let threeinput = document.querySelector('.threeinput');
-let fourinput = document.querySelector('.fourinput');
-let fiveinput = document.querySelector('.fiveinput');
-let Name = document.querySelector('.Name')
-let PHONE = document.querySelector('.PHONE');
-let email = document.querySelector('.email');
-let Subject = document.querySelector('.Subject');
-let Message = document.querySelector('.Message');
 let changeword = "Mohmed Mahmoud"
 let i = 1;
 
-fristbtn.onclick = ()=>{
-  if(oneinput.value  !==''){
-   Name.textContent = ''
-  }else{
-   Name.textContent = 'Name is required'
-  }
-}
-
-let sui = 'Hellow'
-fristbtn.onclick = ()=>{
-  if(twoinput.value  !==''){
-   PHONE.textContent = ''
-  }else{
-   PHONE.textContent = 'PHONE is required'
-  }
-}
-fristbtn.onclick = ()=>{
-  if(threeinput.value  !==''){
-   email.textContent = ''
-  }else{
-   email.textContent = 'invalid email address'
-  }
-}
-fristbtn.onclick = ()=>{
-  if(fourinput.value  !==''){
-   Subject.textContent = ''
-  }else{
-   Subject.textContent = 'Subject is required'
-  }
-}
-fristbtn.onclick = ()=>{
-  if(fiveinput.value  !==''){
-   Message.textContent = ''
-  }else{
-   Message.textContent = 'Message is required'
-  }
-}
-oneinput.onclick = ()=>{
- oneinput.classList.add('hoverinput');
- twoinput.classList.remove('hoverinput');
- threeinput.classList.remove('hoverinput');
- fourinput.classList.remove('hoverinput');
- fiveinput.classList.remove('hoverinput');
-}
-twoinput.onclick = ()=>{
- oneinput.classList.remove('hoverinput');
- twoinput.classList.add('hoverinput');
- threeinput.classList.remove('hoverinput');
- fourinput.classList.remove('hoverinput');
- fiveinput.classList.remove('hoverinput');
-}
-threeinput.onclick = ()=>{
- oneinput.classList.remove('hoverinput');
- twoinput.classList.remove('hoverinput');
- threeinput.classList.add('hoverinput');
- fourinput.classList.remove('hoverinput');
- fiveinput.classList.remove('hoverinput');
-}
-fourinput.onclick = ()=>{
- oneinput.classList.remove('hoverinput');
- twoinput.classList.remove('hoverinput');
- threeinput.classList.remove('hoverinput');
- fourinput.classList.add('hoverinput');
- fiveinput.classList.remove('hoverinput');
-}
-fiveinput.onclick = ()=>{
- oneinput.classList.remove('hoverinput');
- twoinput.classList.remove('hoverinput');
- threeinput.classList.remove('hoverinput');
- fourinput.classList.remove('hoverinput');
- fiveinput.classList.add('hoverinput');
-}
 
 menu.onclick = () =>{
    ulitems.classList.toggle('add');
@@ -136,16 +50,4 @@ window.onload = () =>{
   }else{
    offline()
   }
-}
-window.addEventListener("onLine",()=>{
- onLine();
-})
-window.addEventListener("offline",()=>{
- offline();
-})
-function onLine(){
- offline.classList.add('offlinenow');
-}
-function offline(){
- offline.classList.remove('offlinenow');
 }
